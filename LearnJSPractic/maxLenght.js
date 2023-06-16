9795ff904d924a366fd80c5df6174070f6253375
Natalia-Strizhneva <homosum07@gmail.com>
2023-06-16 17:47:54 +0300

    delete

--- LearnJSPractic/maxLenght.js
+++
@@ -1,6 +0,0 @@
-/* Проверить длину строки и, если она превосходит maxlength, заменяет конец str на "…", 
-так, чтобы её длина стала равна maxlength. */
-
-function truncate(str, maxlength) {
-  return str.length > maxlength ? str.slice(0, maxlength - 1) + "…" : str;
-}
--- LearnJSPractic/rangeArr.js
+++
@@ -1,9 +0,0 @@
-let arr = ["HTML", "JavaScript", "CSS"];
-let sorted = copySorted(arr);
-
-function copySorted(arr) {
-  return arr.slice().sort();
-}
-
-alert(sorted); // CSS, HTML, JavaScript
-alert(arr); // HTML, JavaScript, CSS (без изменений)
